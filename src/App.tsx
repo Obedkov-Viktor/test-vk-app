bee06494a3822cfcbda28bd073d031d2884b5141
Obedkov-Viktor <Obedkov.2018@yandex.ru>
2025-01-17 18:22:45 +0300

    feat(app): add EventEmitterDemo component integration
    - Add EventEmitterDemo component to main App
    - Add visual separator between components
    - Maintain existing RepositoryList functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import RepositoryList from './components/RepositoryList';
+import EventEmitterDemo from "./components/EventEmitterDemo.tsx";
 
 const App: React.FC = () => {
-  return (
-    <div>
-      <h1 style={{ textAlign: 'center' }}>GitHub Repositories</h1>
-      <RepositoryList />
-    </div>
-  );
+    return (
+        <div>
+            <h1 style={{textAlign: 'center'}}>GitHub Repositories</h1>
+            <EventEmitterDemo/>
+            <hr style={{margin: '20px 0 '}}/>
+            <RepositoryList/>
+        </div>
+    );
 };
 
 export default App;
